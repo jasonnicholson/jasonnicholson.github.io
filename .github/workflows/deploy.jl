9da077ea8177f12a9367258d1653583eb4779638
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2025-11-20 18:53:39 -0500

    Added a deploy.jl script

---
+++ .github/workflows/deploy.jl
@@ -0,0 +1,33 @@
+# Note, the worktree method is helpful because LFS items are committed
+# as actual files and not pointers. 
+
+# Creates a local gh-pages branch with no files
+# This must be done before building the site
+run(`git worktree add --orphan -B gh-pages __site`)
+
+# Get the current git commit hash
+current_hash = readchomp(`git rev-parse --short HEAD`);
+
+# NOTE
+#   The steps below ensure that NodeJS and Franklin are loaded then it
+#   installs highlight.js which is needed for the prerendering step
+#   (code highlighting + katex prerendering).
+#   Then the environment is activated and instantiated to install all
+#   Julia packages which may be required to successfully build your site.
+#   The last line should be `optimize()` though you may want to give it
+#   specific arguments, see the documentation or ?optimize in the REPL.
+# Build the site
+using Pkg;
+Pkg.activate(".");
+Pkg.instantiate();
+using NodeJS;
+run(`$(npm_cmd()) install highlight.js`);
+using Franklin;
+optimize();
+
+# Deploy the site
+cd("__site") do
+    run(`git add .`);
+    run(`git commit -m "Deploy to gh-pages $current_hash"`);
+    run(`git push --force origin gh-pages`);
+end
