ff3aa1c4d25fe47de92291b85cae1f4a6712de4a
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2025-11-22 19:47:02 -0500

    moving vba.min.js to the right place

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const src = path.resolve(__dirname, '..', 'node_modules', 'highlightjs-vba', 'dist', 'vba.min.js');
-const destDir = path.resolve(__dirname, '..', 'libs', 'highlight');
+const destDir = path.resolve(__dirname, '..', '_libs', 'highlight');
 const dest = path.join(destDir, 'vba.min.js');
 
 await fs.mkdir(destDir, { recursive: true });
